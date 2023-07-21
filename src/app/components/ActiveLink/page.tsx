5f43dd20adacf13c9e1b21c1c7c004643be67374
VladmirBravo <vladmirobravo@gmail.com>
2023-07-21 09:15:24 +0100

    Responsividade Mobile11

--- src/app/components/ActiveLink/page.tsx
+++ src/app/components/ActiveLink/page.tsx
@@ -2,12 +2,12 @@ import  { ReactElement, cloneElement }  from 'react';
 import { usePathname } from 'next/navigation'
 import Link, {  LinkProps } from 'next/link'
 
-export interface ActiveLinkProps extends LinkProps{
+export default interface ActiveLinkProps extends LinkProps{
   children: ReactElement;
   activeClassName: string;
 }
 
-export default function ActiveLink ({ children, activeClassName, ...rest }: ActiveLinkProps){
+export function ActiveLink ({ children, activeClassName, ...rest }: ActiveLinkProps){
   const pathname = usePathname()
   //Se a rota/pagina que estamos a acessar for igual ao link então ativamos a class
   const className = pathname === rest.href ? activeClassName : '';
